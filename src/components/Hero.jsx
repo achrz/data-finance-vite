6c13693a0470b9fcea561b46606a358f2f04c117
achmadrizky486 <achmadrizky486@gmail.com>
2023-07-24 14:42:21 +0700

    feat : init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
   return (
     <div className="text-white">
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-        <p className="text-[#00df9a] font-bold">Capstone 11</p>
+        <p className="text-[#00df9a] font-bold">Hai</p>
         <h1 className="md:text-7xl md:py-6 sm:text-6xl text-4xl font-bold">
           Build with{" "}
           <Typed
-            strings={["Love.", "Stupidity."]}
+            strings={["Love.", "Heart."]}
             typeSpeed={140}
             backSpeed={50}
             loop
@@ -17,11 +17,11 @@ const Hero = () => {
         </h1>
         <div>
           <p className="md:text-5xl sm:text-4xl text-xl font-bold">
-            Awokwokwokwowk
+            Data Finance
           </p>
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500 mt-5">
-          Gak tau mau nulis apa
+          Get Your Best Data Finance Manager
         </p>
         <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
           Get Started
